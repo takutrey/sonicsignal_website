8a6e5d2df90aaef4b18a7e4876f557370fe2a6ae
takutrey <takuganda@mail.com>
2025-06-09 11:15:53 +0200

    Blogs working and new build file

---
+++ admin/dist/assets/@tabler.CjN-xnUV.js
@@ -0,0 +1,61 @@
+import{r as e}from"./react.DRiZ1M9A.js";/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var M={outline:{xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"},filled:{xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"currentColor",stroke:"none"}};/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=(v,r,n,g)=>{const o=e.forwardRef(({color:d="currentColor",size:s=24,stroke:p=2,title:h,className:l,children:t,...i},k)=>e.createElement("svg",{ref:k,...M[v],width:s,height:s,className:["tabler-icon",`tabler-icon-${r}`,l].join(" "),strokeWidth:p,stroke:d,...i},[h&&e.createElement("title",{key:"svg-title"},h),...g.map(([c,y])=>e.createElement(c,y)),...Array.isArray(t)?t:[t]]));return o.displayName=`${n}`,o};/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var u=a("outline","category-2","IconCategory2",[["path",{d:"M14 4h6v6h-6z",key:"svg-0"}],["path",{d:"M4 14h6v6h-6z",key:"svg-1"}],["path",{d:"M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0",key:"svg-2"}],["path",{d:"M7 7m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0",key:"svg-3"}]]);/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var I=a("outline","dashboard","IconDashboard",[["path",{d:"M12 13m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0",key:"svg-0"}],["path",{d:"M13.45 11.55l2.05 -2.05",key:"svg-1"}],["path",{d:"M6.4 20a9 9 0 1 1 11.2 0z",key:"svg-2"}]]);/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var w=a("outline","dots-vertical","IconDotsVertical",[["path",{d:"M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0",key:"svg-0"}],["path",{d:"M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0",key:"svg-1"}],["path",{d:"M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0",key:"svg-2"}]]);/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var C=a("outline","folder-code","IconFolderCode",[["path",{d:"M11 19h-6a2 2 0 0 1 -2 -2v-11a2 2 0 0 1 2 -2h4l3 3h7a2 2 0 0 1 2 2v4",key:"svg-0"}],["path",{d:"M20 21l2 -2l-2 -2",key:"svg-1"}],["path",{d:"M17 17l-2 2l2 2",key:"svg-2"}]]);/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var f=a("outline","list-details","IconListDetails",[["path",{d:"M13 5h8",key:"svg-0"}],["path",{d:"M13 9h5",key:"svg-1"}],["path",{d:"M13 15h8",key:"svg-2"}],["path",{d:"M13 19h5",key:"svg-3"}],["path",{d:"M3 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z",key:"svg-4"}],["path",{d:"M3 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z",key:"svg-5"}]]);/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var b=a("outline","logout","IconLogout",[["path",{d:"M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2",key:"svg-0"}],["path",{d:"M9 12h12l-3 -3",key:"svg-1"}],["path",{d:"M18 15l3 -3",key:"svg-2"}]]);/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var x=a("outline","news","IconNews",[["path",{d:"M16 6h3a1 1 0 0 1 1 1v11a2 2 0 0 1 -4 0v-13a1 1 0 0 0 -1 -1h-10a1 1 0 0 0 -1 1v12a3 3 0 0 0 3 3h11",key:"svg-0"}],["path",{d:"M8 8l4 0",key:"svg-1"}],["path",{d:"M8 12l4 0",key:"svg-2"}],["path",{d:"M8 16l4 0",key:"svg-3"}]]);/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var D=a("outline","shopping-bag-check","IconShoppingBagCheck",[["path",{d:"M11.5 21h-2.926a3 3 0 0 1 -2.965 -2.544l-1.255 -8.152a2 2 0 0 1 1.977 -2.304h11.339a2 2 0 0 1 1.977 2.304l-.5 3.248",key:"svg-0"}],["path",{d:"M9 11v-5a3 3 0 0 1 6 0v5",key:"svg-1"}],["path",{d:"M15 19l2 2l4 -4",key:"svg-2"}]]);/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var L=a("outline","shopping-cart","IconShoppingCart",[["path",{d:"M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0",key:"svg-0"}],["path",{d:"M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0",key:"svg-1"}],["path",{d:"M17 17h-11v-14h-2",key:"svg-2"}],["path",{d:"M6 5l14 1l-1 7h-13",key:"svg-3"}]]);/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var z=a("outline","users","IconUsers",[["path",{d:"M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0",key:"svg-0"}],["path",{d:"M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2",key:"svg-1"}],["path",{d:"M16 3.13a4 4 0 0 1 0 7.75",key:"svg-2"}],["path",{d:"M21 21v-2a4 4 0 0 0 -3 -3.85",key:"svg-3"}]]);export{w as I,b as a,I as b,f as c,L as d,D as e,z as f,u as g,x as h,C as i};
---
+++ admin/dist/assets/use-sync-external-store.kb0GEi_R.js
@@ -0,0 +1,17 @@
+import{b as z}from"./react.DRiZ1M9A.js";var x={exports:{}},R={};/**
+ * @license React
+ * use-sync-external-store-with-selector.production.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var q;function I(){if(q)return R;q=1;var n=z();function d(o,a){return o===a&&(o!==0||1/o===1/a)||o!==o&&a!==a}var m=typeof Object.is=="function"?Object.is:d,h=n.useSyncExternalStore,E=n.useRef,p=n.useEffect,y=n.useMemo,b=n.useDebugValue;return R.useSyncExternalStoreWithSelector=function(o,a,l,e,r){var t=E(null);if(t.current===null){var i={hasValue:!1,value:null};t.current=i}else i=t.current;t=y(function(){function f(c){if(!_){if(_=!0,v=c,c=e(c),r!==void 0&&i.hasValue){var s=i.value;if(r(s,c))return S=s}return S=c}if(s=S,m(v,c))return s;var W=e(c);return r!==void 0&&r(s,W)?(v=c,s):(v=c,S=W)}var _=!1,v,S,j=l===void 0?null:l;return[function(){return f(a())},j===null?void 0:function(){return f(j())}]},[a,l,e,r]);var u=h(o,t[0],t[1]);return p(function(){i.hasValue=!0,i.value=u},[u]),b(u),u},R}var U;function M(){return U||(U=1,x.exports=I()),x.exports}var $=M(),w={exports:{}},V={};/**
+ * @license React
+ * use-sync-external-store-shim.production.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var D;function g(){if(D)return V;D=1;var n=z();function d(e,r){return e===r&&(e!==0||1/e===1/r)||e!==e&&r!==r}var m=typeof Object.is=="function"?Object.is:d,h=n.useState,E=n.useEffect,p=n.useLayoutEffect,y=n.useDebugValue;function b(e,r){var t=r(),i=h({inst:{value:t,getSnapshot:r}}),u=i[0].inst,f=i[1];return p(function(){u.value=t,u.getSnapshot=r,o(u)&&f({inst:u})},[e,t,r]),E(function(){return o(u)&&f({inst:u}),e(function(){o(u)&&f({inst:u})})},[e]),y(t),t}function o(e){var r=e.getSnapshot;e=e.value;try{var t=r();return!m(e,t)}catch{return!0}}function a(e,r){return r()}var l=typeof window>"u"||typeof window.document>"u"||typeof window.document.createElement>"u"?a:b;return V.useSyncExternalStore=n.useSyncExternalStore!==void 0?n.useSyncExternalStore:l,V}var O;function G(){return O||(O=1,w.exports=g()),w.exports}var k=G();export{k as s,$ as w};
